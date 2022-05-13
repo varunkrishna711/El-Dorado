5d83ed412ea9e02b74b60b25af17f355ba71c1c1
Ashik-Jose <ashiksholly@gmail.com>
2022-05-13 19:17:11 +0530

    Ashik Jose

---
+++ frontend/src/Competitions/Competitions.js
@@ -0,0 +1,59 @@
+import React from "react";
+import Showcomp from "./Showcomp";
+import './Competitions.css';
+
+const Competitions=()=>{
+    return(
+        <div className="imgbody">
+            <div class="header">
+			<h1 id="heading">COMPETITIONS</h1>
+		</div>
+        <Showcomp
+        image="https://www.google.com/imgres?imgurl=https%3A%2F%2Fscholarblogs.emory.edu%2Fpostcolonialstudies%2Ffiles%2F2014%2F06%2F449px-Kathakalidancer.jpg&imgrefurl=https%3A%2F%2Fscholarblogs.emory.edu%2Fpostcolonialstudies%2F2014%2F06%2F21%2Fkathakali%2F&tbnid=oOjO2UGJPx0RpM&vet=12ahUKEwjRuMjew9z3AhXWgGMGHQt1BMYQMygLegUIARD-AQ..i&docid=4ve9lBnkOsR9QM&w=449&h=599&q=kathakali%20images&ved=2ahUKEwjRuMjew9z3AhXWgGMGHQt1BMYQMygLegUIARD-AQ"
+        name="All Kerala Kathakali Competition"
+        venue= "Trivandrum Town Square"
+        date= "January 30"
+        prize="20K"
+      />
+
+    <Showcomp
+        image = "https://www.google.com/imgres?imgurl=http%3A%2F%2Fwww.schoolchalao.com%2Fimg%2Fextraimages%2Fschool-chalao-chakyar-dance1.jpg&imgrefurl=http%3A%2F%2Fwww.schoolchalao.com%2Fbasic-education%2Fshow-results%2Findian-folk-dance%2Fchakyar-koothu-dance-kerala&tbnid=du9Wnl74BO57ZM&vet=12ahUKEwiGkqzTxNz3AhUuzqACHRa4C-EQMygIegUIARDvAQ..i&docid=3JzTBflLaBafxM&w=282&h=185&q=chakyarkuthu%20images&ved=2ahUKEwiGkqzTxNz3AhUuzqACHRa4C-EQMygIegUIARDvAQ"
+        name="All India Chakyarkuthu Competition"
+        venue= "Marine Drive"
+        date= "February 14,2022"
+        prize="15K"
+      />
+
+    <Showcomp
+        image="https://www.google.com/imgres?imgurl=https%3A%2F%2Fwww.keralatourism.org%2Fimages%2Fartforms%2Flarge%2Fkalaripayattu20131111114353_27_1.jpg&imgrefurl=https%3A%2F%2Fwww.keralatourism.org%2Fartforms%2Fkalaripayattu-martial-art-kerala%2F27&tbnid=IO2Y1VpTOeLaiM&vet=12ahUKEwiKptSjxdz3AhVC_TgGHdazDsIQMygBegUIARDfAQ..i&docid=gL9o9Uwi32Z6DM&w=1920&h=1080&q=kalari%20images&ved=2ahUKEwiKptSjxdz3AhVC_TgGHdazDsIQMygBegUIARDfAQ"
+        name="District level kalari championship"
+        venue= "Pulimada Beachresort"
+        date= "February 28,2022"
+        prize="30K"
+      />
+      <Showcomp
+        image="https://www.google.com/imgres?imgurl=https%3A%2F%2Fwww.sahapedia.org%2Fsites%2Fdefault%2Ffiles%2Fstyles%2Fsp_page_banner_800x800%2Fpublic%2FIMG-20180425-WA0122.jpg%3Fitok%3Dg2X3KW0J&imgrefurl=https%3A%2F%2Fwww.sahapedia.org%2Fmargamkali-1&tbnid=hk7NOWm9_ordQM&vet=12ahUKEwiE__ORxtz3AhV-jNgFHcXrAhsQMygBegUIARC8AQ..i&docid=1JtAuir9jSIy_M&w=800&h=800&q=margamkali%20images&ved=2ahUKEwiE__ORxtz3AhV-jNgFHcXrAhsQMygBegUIARC8AQ"
+        name="Amateur margamkali tournament"
+        venue= "Lulu Mall"
+        date= "March 27,2022"
+        prize="25K"
+      />
+      <Showcomp
+        image="https://www.google.com/imgres?imgurl=https%3A%2F%2Fmedia.istockphoto.com%2Fphotos%2Ftiger-dance-of-kerala-picture-id849235044%3Fk%3D20%26m%3D849235044%26s%3D612x612%26w%3D0%26h%3DC3t3EIIA2O66HQERebF0XpCUdj7ZYJuzTftyIZ6_U0I%3D&imgrefurl=https%3A%2F%2Fwww.istockphoto.com%2Fphotos%2Fpulikali&tbnid=z28zRFU4e6fTtM&vet=12ahUKEwiJ1pTTxtz3AhWxktgFHVwuA2oQMygEegUIARDyAQ..i&docid=8pqzzsGRaUrojM&w=612&h=408&q=pulikaliimages&ved=2ahUKEwiJ1pTTxtz3AhWxktgFHVwuA2oQMygEegUIARDyAQ"
+        name="National level pulikali championship"
+        venue= "Decathlon Convention center"
+        date= "April 7, 2020"
+        prize="10K"
+      />
+      <Showcomp
+        image="https://www.google.com/imgres?imgurl=https%3A%2F%2Fi.pinimg.com%2Foriginals%2F5a%2Ff6%2F43%2F5af6436f5efa34c49376a8f9b91e7ac1.jpg&imgrefurl=https%3A%2F%2Fwww.pinterest.com%2Fpin%2F73535406386448134%2F&tbnid=uUFg2E2ikMQF_M&vet=12ahUKEwiezqeIx9z3AhW1jNgFHe8aB-UQMyg-egQIARBt..i&docid=bSwZjhx2jBrlzM&w=480&h=360&q=oppana%20images&ved=2ahUKEwiezqeIx9z3AhW1jNgFHe8aB-UQMyg-egQIARBt"
+        name="Oppana intercollege Competition"
+        venue= "Camlot"
+        date= "May 21, 2020"
+        prize="5K"
+      />
+        </div>
+    );
+}
+
+export default Competitions;
