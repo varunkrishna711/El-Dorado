6a2b8f6d4aa5d423d2790bbcf3f391fa813e494f
jaidan22 <jaidansleeba22@gmail.com>
2022-05-14 14:42:58 +0530

    changed amount

--- api/Routes/razorpay.js
+++ api/Routes/razorpay.js
@@ -1,69 +1,67 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
-const Razorpay = require('razorpay');
-const request = require('request');
+const Razorpay = require("razorpay");
+const request = require("request");
 
-const keys = require('./keys');
-const dotenv = require('dotenv');
+const keys = require("./keys");
+const dotenv = require("dotenv");
 dotenv.config();
 
 const razorInstance = new Razorpay({
-  key_id : process.env.key_id,
-  key_secret : process.env.key_secret
+  key_id: process.env.key_id,
+  key_secret: process.env.key_secret,
 });
 
-router.get("/book",(req,res)=>{
-  try{
-    const options ={
-      amount : 10*100,
-      currency : "INR",
+router.get("/book/:price", (req, res) => {
+  try {
+    // console.log(req.params);
+    const options = {
+      amount: req.params.price,
+      currency: "INR",
       receipt: "receipt#1",
       payment_capture: 0, //1
-
     };
-    razorInstance.orders.create(options,async function(err,order){
-      if(err){
+    razorInstance.orders.create(options, async function (err, order) {
+      if (err) {
         return res.status(500).json({
-          message: "Something error!s"
-        })
+          message: "Something error!s",
+        });
       }
-      return res.status(200).json(order)
+      return res.status(200).json(order);
     });
-  }
-  catch(err){
+  } catch (err) {
     return res.status(500).json({
-      message: "Something error!s"
-    })
+      message: "Something error!s",
+    });
   }
 });
 
-router.post("/capture/:paymentId",(req,res)=>{
-  try{
+router.post("/capture/:paymentId", (req, res) => {
+  try {
     return request(
       {
-        method : "POST",
-        url : `https://${keys.razorIdkey}:${keys.razorIdSecret}@api.razorpay.com/v1/payments/${req.params.paymentId}/capture`,
-        form:{
-          amount : 10 *100,
-          currency: "INR"
+        method: "POST",
+        url: `https://${keys.razorIdkey}:${keys.razorIdSecret}@api.razorpay.com/v1/payments/${req.params.paymentId}/capture`,
+        form: {
+          amount: 20 * 200,
+          currency: "INR",
         },
       },
-      async function(err,response,body){
-        if(err){
+      async function (err, response, body) {
+        if (err) {
           return res.status(500).json({
-            message: "Something error!s"
-          })
+            message: "Something wrong",
+          });
         }
-        return res.status(200).json(body)
+        return res.status(200).json(body);
       }
-    )
-  }
-  catch(err){
+    );
+  } catch (err) {
     return res.status(500).json({
-      message: err.message
-    })
+      message: err.message,
+    });
   }
-})
+});
 
 module.exports = router;
