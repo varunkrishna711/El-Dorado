50d2a4479df05e1e772f695f93d65ead0ec640d3
jaidan22 <jaidansleeba22@gmail.com>
2022-05-14 10:43:10 +0530

    new commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,11 @@
 // import './App.css';
-import Header from './Header';
-import Home from './Home';
-import Footer from './Footer';
+import Header from "./components/Header";
+import Home from "./components/Home";
+import Footer from "./components/Footer";
 // import Checkout from './Checkout';
 // import Login from './Login';
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
-import PopularShows from './PopularShows';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import PopularShows from "./components/PopularShows";
 // import { useEffect } from 'react';
 // import {auth} from './firebase';
 // import {useStateValue} from './StateProvider';
@@ -15,12 +15,18 @@ function App() {
     <Router>
       <div className="app">
         <Routes>
-            <Route path="/" element={<><Header /> <PopularShows /> <Footer/></>} />
+          <Route
+            path="/"
+            element={
+              <>
+                <Header /> <PopularShows /> <Footer />
+              </>
+            }
+          />
         </Routes>
       </div>
     </Router>
   );
-
 }
 
 export default App;
